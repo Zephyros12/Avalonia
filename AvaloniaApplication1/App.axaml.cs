8707b927d271dca33d277bc6c28ea48f58fdf65f
임규성 <rimrbtjd12@gmail.com>
2025-04-09 13:41:56 +0900

    avalonia를 활용한 카메라 연결

--- AvaloniaApplication1/App.axaml.cs
+++ AvaloniaApplication1/App.axaml.cs
@@ -1,7 +1,6 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using AvaloniaApplication1.ViewModels;
 using AvaloniaApplication1.Views;
 
 namespace AvaloniaApplication1
@@ -17,14 +16,10 @@ namespace AvaloniaApplication1
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
+                desktop.MainWindow = new MainWindow();
             }
 
             base.OnFrameworkInitializationCompleted();
         }
-
     }
 }
--- AvaloniaApplication1/Views/MainWindow.axaml.cs
+++ AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -1,12 +1,93 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Threading;
+using Basler.Pylon;
+using System;
+using System.Runtime.InteropServices;
 
 namespace AvaloniaApplication1.Views
 {
     public partial class MainWindow : Window
     {
+        private Camera? _camera;
+        private PixelDataConverter? _converter;
+
         public MainWindow()
         {
             InitializeComponent();
+            Opened += OnOpened;
+            Closed += OnClosed;
+        }
+
+        private void OnOpened(object? sender, EventArgs e)
+        {
+            InitializeCamera();
+        }
+
+        private void OnClosed(object? sender, EventArgs e)
+        {
+            ShutdownCamera();
+        }
+
+        private void InitializeCamera()
+        {
+            _camera = new Camera();
+            _camera.Open();
+
+            _converter = new PixelDataConverter
+            {
+                OutputPixelFormat = PixelType.BGRA8packed
+            };
+
+            _camera.StreamGrabber.ImageGrabbed += OnImageGrabbed;
+            _camera.StreamGrabber.Start(GrabStrategy.LatestImages, GrabLoop.ProvidedByStreamGrabber);
+        }
+
+        private void ShutdownCamera()
+        {
+            if (_camera is not null)
+            {
+                _camera.StreamGrabber.Stop();
+                _camera.Close();
+                _camera.Dispose();
+                _camera = null;
+            }
+        }
+
+        private void OnImageGrabbed(object? sender, ImageGrabbedEventArgs e)
+        {
+            if (!e.GrabResult.GrabSucceeded || _converter is null)
+            {
+                return;
+            }
+
+            IGrabResult grabResult = e.GrabResult;
+            int width = grabResult.Width;
+            int height = grabResult.Height;
+            int stride = width * 4;
+
+            byte[] buffer = new byte[_converter.GetBufferSizeForConversion(grabResult)];
+            _converter.Convert(buffer, grabResult);
+
+            GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            IntPtr pointer = handle.AddrOfPinnedObject();
+
+            Dispatcher.UIThread.Post(() =>
+            {
+                var bitmap = new Bitmap(
+                    PixelFormat.Bgra8888,
+                    AlphaFormat.Unpremul,
+                    pointer,
+                    new PixelSize(width, height),
+                    new Vector(96, 96),
+                    stride);
+
+                CameraImage.Source = bitmap;
+            });
+
+            handle.Free();
         }
     }
 }
